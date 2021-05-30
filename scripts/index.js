994e66f763502290975d9884e3e265c54a3b129a
Matvey Pakhomov <matwick1@yandex.ru>
2021-05-30 17:22:28 +0400

    fix: поправить пути до файлов шрифтов

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,23 @@ let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 let profileName = profile.querySelector('.profile__title');
 let profileJob = profile.querySelector('.profile__subtitle');
-let popup =  document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-button');
-let nameInput = popup.querySelector('.popup__profile-name');
-let jobInput = popup.querySelector('.popup__profile-job');
+let nameInput = popup.querySelector('.popup_profile-name');
+let jobInput = popup.querySelector('.popup_profile-job');
 let formElement = popup.querySelector('.popup__form');
 
-function toggleClass() {
-  popup.classList.toggle('popup_opened');
+function popupOpened() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
+
+function popupClosed() {
+  popup.classList.remove('popup_opened');
+}
+
+// попытка сделать закрытие попапа по щелчку на свободное пространство, надо доработать
 
 // popup.addEventListener('click', function(event) {
 //   if (event.target === event.currentTarget) {
@@ -22,15 +28,15 @@ function toggleClass() {
 //   }
 // })
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  toggleClass();
+  popupClosed();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-editButton.addEventListener('click', toggleClass);
+editButton.addEventListener('click', popupOpened);
 
-closeButton.addEventListener('click', toggleClass);
+closeButton.addEventListener('click', popupClosed);
